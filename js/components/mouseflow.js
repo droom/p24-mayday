d37fad3d82bc0162458ae1b255b256041210b125
Chris Droom <chrisdroom@gmail.com>
2024-12-17 01:20:29 +0000

    cleanup

--- js/components/mouseflow.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    var _mfq = _mfq || [];
-    (function() {
-        var mf = document.createElement("script");
-        mf.type = "text/javascript"; mf.async = true;
-        mf.src = "//cdn.mouseflow.com/projects/7c2dbb43-448a-4197-ab50-909d287eec94.js";
-        document.getElementsByTagName("head")[0].appendChild(mf);
-    })();
-</script>
